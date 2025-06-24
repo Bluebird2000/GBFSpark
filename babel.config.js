8e7ffccdb964a8bee38ee0c1f9dfba82839ffe06
Bluebird2000 <lateefahmad3868@gmail.com>
2025-06-24 15:12:29 +0100

    chore: configure Babel module resolver for absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': '.',
+          '@src': './src',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@template': './src/components/template',
+          test: './test',
+          underscores: 'lodash',
+        },
+      },
+    ],
+  ],
 };
